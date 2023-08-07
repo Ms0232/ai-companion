b614881302d9696e7c1e0af9392e260febde180c
Ms0232 <180540106059@darshan.ac.in>
2023-08-07 19:21:15 +0530

    chat header done

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -2,9 +2,24 @@
 
 import { Companion, Message } from "@prisma/client";
 import { Button } from "./ui/button";
-import { ChevronLeft, MessagesSquare } from "lucide-react";
+import {
+  ChevronLeft,
+  Edit,
+  MessagesSquare,
+  MoreVertical,
+  Trash,
+} from "lucide-react";
 import { useRouter } from "next/navigation";
 import BotAvatar from "./bot-avatar";
+import { useUser } from "@clerk/nextjs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useToast } from "./ui/use-toast";
+import axios from "axios";
 
 interface ChatHeaderProps {
   companion: Companion & {
@@ -17,6 +32,21 @@ interface ChatHeaderProps {
 
 const ChatHeader = ({ companion }: ChatHeaderProps) => {
   const router = useRouter();
+  const { user } = useUser();
+  const { toast } = useToast();
+
+  const onDelete = async () => {
+    try {
+      await axios.delete(`/api/companion/${companion.id}`);
+      toast({
+        description: "Success.",
+      });
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      toast({ description: "Something went wrong", variant: "destructive" });
+    }
+  };
   return (
     <div className="flex w-full justify-between items-center border-b border-primary/10 pb-4">
       <div className="flex gap-x-2 items-center">
@@ -37,6 +67,27 @@ const ChatHeader = ({ companion }: ChatHeaderProps) => {
           </p>
         </div>
       </div>
+      {user?.id === companion.userId && (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon">
+              <MoreVertical />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={() => router.push(`/companion/${companion.id}`)}
+            >
+              <Edit className="w-4 h-4 mr-2" />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={onDelete}>
+              <Trash className="w-4 h-4 mr-2" />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
     </div>
   );
 };
